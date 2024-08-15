e4d0d0e19b122b8ff704663aeb86bd111e72464b
Richard Sustek <ennspassion@gmail.com>
2024-08-15 10:18:26 +0200

    updates eslint confing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.r
         '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-empty-object-type': 'off',
         '@typescript-eslint/no-unsafe-return': 'off',
         '@typescript-eslint/prefer-promise-reject-errors': 'off'
