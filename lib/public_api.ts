066dd94f5a7d752b1ab805e931e31cf6528b3daa
Richard Sustek <ennspassion@gmail.com>
2025-04-14 12:53:21 +0200

    Update public_api.ts

--- lib/public_api.ts
+++ lib/public_api.ts
@@ -9,6 +9,8 @@ export {
     type HttpServiceStatus
 } from './http/http.models.js';
 export { defaultHttpService } from './http/http.service.js';
-export type { Header, HttpMethod, RetryStrategyOptions, SDKInfo } from './models/core.models.js';
+export type { Header, HttpMethod, RetryStrategyOptions, SdkErrorData, SDKInfo } from './models/core.models.js';
+export type { JsonArray, JsonObject, JsonValue } from './models/json.models.js';
+export { getSdkIdHeader } from './utils/header.utils.js';
 export { toRequiredRetryStrategyOptions } from './utils/retry.utils.js';
 export { createVersionFile, deleteFolderRecursive } from './utils/script.utils.js';
