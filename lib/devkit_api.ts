7f9ea5084a6ade4343ecd71b8c229b38bda780c9
Richard Sustek <ennspassion@gmail.com>
2025-05-29 13:59:40 +0200

    extends devkit api

--- lib/devkit_api.ts
+++ lib/devkit_api.ts
@@ -1,5 +1,7 @@
 /**
  * Devkit API - used for development purposes in
  */
+export type { FetchResponse } from './devkit/devkit.models.js';
+export { deleteFolderRecursive } from './devkit/script.utils.js';
 export type { getFakeBlob, getFetchBlobMock, getFetchJsonMock } from './devkit/test.utils.js';
 export { replaceSdkVersionPlaceholder } from './utils/sdk-version.utils.js';
