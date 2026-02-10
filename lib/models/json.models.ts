d5e748dd0659d5fdfe7271e3defacf8aea8f7a1a
Richard Sustek <ennspassion@gmail.com>
2026-02-10 17:51:00 +0100

    Update json.models.ts

--- lib/models/json.models.ts
+++ lib/models/json.models.ts
@@ -2,8 +2,8 @@ import type { EmptyObject } from "./utility.models.js";
 
 export type JsonValue = string | number | null | boolean | JsonObject | JsonArray | EmptyObject;
 
-export interface JsonObject {
+export type JsonObject = {
 	readonly [property: string]: JsonValue;
-}
+};
 
 export type JsonArray = readonly JsonValue[];
