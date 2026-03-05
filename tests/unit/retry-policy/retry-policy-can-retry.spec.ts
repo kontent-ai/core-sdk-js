0cc9e88d5eacd1eaa80bfd2046bbc720dd88b974
Richard Sustek <ennspassion@gmail.com>
2026-03-05 13:56:53 +0100

    Improves retry specs

--- tests/unit/retry-policy/retry-policy-can-retry.spec.ts
+++ tests/unit/retry-policy/retry-policy-can-retry.spec.ts
@@ -1,13 +1,11 @@
 import { describe, expect, it } from "vitest";
 import { getDefaultHttpService } from "../../../lib/http/http.service.js";
 import type { RetryStrategyOptions } from "../../../lib/models/core.models.js";
-import type { FetchResponse } from "../../../lib/testkit/testkit.models.js";
 import { resolveDefaultRetryStrategyOptions } from "../../../lib/utils/retry.utils.js";
 
 type TestCase = RetryStrategyOptions & {
 	readonly title: string;
 	readonly expectedRetries: number;
-	readonly fetchResponse: FetchResponse;
 };
 
 const testCases: readonly TestCase[] = [
@@ -16,30 +14,36 @@ const testCases: readonly TestCase[] = [
 		canRetryUnhandledError: resolveDefaultRetryStrategyOptions({}).canRetryUnhandledError,
 		maxRetries: 5,
 		expectedRetries: 0,
-		fetchResponse: {
-			statusCode: 500,
-			json: {},
-		},
 	},
 	{
 		title: `Can't retry if canRetryUnhandledError returns false`,
 		canRetryUnhandledError: () => false,
 		maxRetries: 5,
 		expectedRetries: 0,
-		fetchResponse: {
-			statusCode: 500,
-			json: {},
-		},
 	},
 	{
 		title: "Can retry when unhandled errors are set to be retried via custom callback",
 		canRetryUnhandledError: () => true,
 		maxRetries: 1,
 		expectedRetries: 1,
-		fetchResponse: {
-			statusCode: 500,
-			json: {},
-		},
+	},
+	{
+		title: "Can retry, but should not retry because max retries is 0",
+		expectedRetries: 0,
+		maxRetries: 0,
+		canRetryUnhandledError: () => true,
+	},
+	{
+		title: "Should retry 2 times",
+		expectedRetries: 2,
+		maxRetries: 2,
+		canRetryUnhandledError: () => true,
+	},
+	{
+		title: "Should retry 5 times",
+		expectedRetries: 5,
+		maxRetries: 5,
+		canRetryUnhandledError: () => true,
 	},
 ];
 
