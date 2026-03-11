5cde492282762343b2dd1ca66bb9a556c8345389
Richard Sustek <ennspassion@gmail.com>
2026-03-11 11:45:55 +0100

    updates extra metadata test

--- tests/unit/queries/query-extra-metadata.spec.ts
+++ tests/unit/queries/query-extra-metadata.spec.ts
@@ -14,6 +14,12 @@ describe("Query extra metadata", async () => {
 		name: "x",
 		value: "y",
 	};
+
+	let mappedContinuationToken: string | undefined;
+	let mappedResponseStatus: number | undefined;
+	let mappedResponseMethod: HttpMethod | undefined;
+	let mappedRequestHeaders: readonly Header[] | undefined;
+
 	const { response } = await createFetchQuery<
 		null,
 		{
@@ -21,27 +27,10 @@ describe("Query extra metadata", async () => {
 		}
 	>({
 		mapMetadata: (response, data) => {
-			it("Continuation token should be the same as the one from the response", () => {
-				expect(data.continuationToken).toStrictEqual(responseContinuationToken);
-			});
-
-			it("Response status code should be the same as the one from the response", () => {
-				expect(response.adapterResponse.status).toStrictEqual(responseStatusCode);
-			});
-
-			it("Response method should be the same as the one from the request", () => {
-				expect(response.method).toStrictEqual(method);
-			});
-
-			it(`Request headers should contain the header '${requestHeader.name}' from the request`, () => {
-				expect(response.requestHeaders.find((m) => m.name === requestHeader.name)).toStrictEqual(requestHeader);
-			});
-
-			it(`Request headers should contain sdk id header`, () => {
-				expect(response.requestHeaders.find((m) => m.name === ("X-KC-SDKID" satisfies CommonHeaderNames))).toStrictEqual(
-					getSdkIdHeader(getTestSdkInfo()),
-				);
-			});
+			mappedContinuationToken = data.continuationToken;
+			mappedResponseStatus = response.adapterResponse.status;
+			mappedResponseMethod = response.method;
+			mappedRequestHeaders = response.requestHeaders;
 
 			return {
 				testExtraMetadata: expectedExtraMetadataValue,
@@ -67,5 +56,27 @@ describe("Query extra metadata", async () => {
 
 	it("Meta should have proper extra metadata", () => {
 		expect(response?.meta.testExtraMetadata).toStrictEqual(expectedExtraMetadataValue);
+	});
+
+	it("Continuation token should be the same as the one from the response", () => {
+		expect(mappedContinuationToken).toStrictEqual(responseContinuationToken);
+	});
+
+	it("Response status code should be the same as the one from the response", () => {
+		expect(mappedResponseStatus).toStrictEqual(responseStatusCode);
+	});
+
+	it("Response method should be the same as the one from the request", () => {
+		expect(mappedResponseMethod).toStrictEqual(method);
+	});
+
+	it(`Request headers should contain the header '${requestHeader.name}' from the request`, () => {
+		expect(mappedRequestHeaders?.find((m) => m.name === requestHeader.name)).toStrictEqual(requestHeader);
+	});
+
+	it("Request headers should contain sdk id header", () => {
+		expect(mappedRequestHeaders?.find((m) => m.name === ("X-KC-SDKID" satisfies CommonHeaderNames))).toStrictEqual(
+			getSdkIdHeader(getTestSdkInfo()),
+		);
 	});
 });
