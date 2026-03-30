94a5f451fc21ed0278f53445c2114085e4132e91
Richard Sustek <ennspassion@gmail.com>
2026-03-30 12:31:07 +0200

    Update getCodenameSchema impl

--- lib/utils/type.utils.ts
+++ lib/utils/type.utils.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
-export function getCodenameSchema<const TCodenames extends readonly string[]>(codenames?: TCodenames): z.ZodType<TCodenames[number]> {
-	return codenames && codenames.length > 0 ? z.enum(codenames) : z.string();
+export function getCodenameSchema<const TCodenames extends string>(codenames: readonly TCodenames[] | undefined): z.ZodType<TCodenames> {
+	return (codenames && codenames.length > 0 ? z.literal(codenames) : z.string()) as z.ZodType<TCodenames>;
 }
