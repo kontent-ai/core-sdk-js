6cd030198ab8e319d74c05e16c83b35640dd2607
Richard Sustek <ennspassion@gmail.com>
2026-04-01 09:42:15 +0200

    exclude tests from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
 		dir: "tests",
 		globals: true,
 		environment: "node",
-		coverage: { provider: "v8", thresholds: { lines: 90, functions: 90, branches: 90 }, exclude: ["**/testkit/**", "**/devkit/**", "vitest-integration.config.ts"] },
+		coverage: {
+			provider: "v8",
+			thresholds: { lines: 90, functions: 90, branches: 90 },
+			exclude: ["**/testkit/**", "**/devkit/**", "**/tests/**"],
+		},
 	},
 	build: {
 		target: "esnext",
