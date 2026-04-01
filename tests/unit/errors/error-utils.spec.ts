1c44f2ca29e0aee00142fb3041be78bd4556be18
Richard Sustek <ennspassion@gmail.com>
2026-04-01 14:07:46 +0200

    Removes another useless tests

--- tests/unit/errors/error-utils.spec.ts
+++ tests/unit/errors/error-utils.spec.ts
@@ -147,40 +147,11 @@ describe("toFriendlyKontentSdkErrorMessage", () => {
 		expect(toFriendlyKontentSdkErrorMessage(baseMessage, error)).toStrictEqual(`${baseMessage} ${kontentErrorResponse.message}`);
 	});
 
-	it("Should append kontentErrorResponse message for 'notFound' with non-null kontentErrorResponse", () => {
-		const error: ErrorDetails = {
-			reason: "notFound",
-			kontentErrorResponse,
-			...responseErrorBase,
-		};
-
-		expect(toFriendlyKontentSdkErrorMessage(baseMessage, error)).toStrictEqual(`${baseMessage} ${kontentErrorResponse.message}`);
-	});
-
 	it("Should return base message for 'invalidResponse' when kontentErrorResponse is undefined", () => {
 		const error: ErrorDetails = {
 			reason: "invalidResponse",
 			kontentErrorResponse: undefined,
 			...responseErrorBase,
-		};
-
-		expect(toFriendlyKontentSdkErrorMessage(baseMessage, error)).toStrictEqual(baseMessage);
-	});
-
-	it("Should return base message for 'notFound' when kontentErrorResponse is undefined", () => {
-		const error: ErrorDetails = {
-			reason: "notFound",
-			kontentErrorResponse: undefined,
-			...responseErrorBase,
-		};
-
-		expect(toFriendlyKontentSdkErrorMessage(baseMessage, error)).toStrictEqual(baseMessage);
-	});
-
-	it("Should return base message for any other reason", () => {
-		const error: ErrorDetails = {
-			reason: "adapterError",
-			originalError: new Error("Network failure"),
 		};
 
 		expect(toFriendlyKontentSdkErrorMessage(baseMessage, error)).toStrictEqual(baseMessage);
