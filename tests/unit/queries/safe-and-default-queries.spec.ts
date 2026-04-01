223fd029f17e09fbc00bf96153c7417a272a8dcd
Richard Sustek <ennspassion@gmail.com>
2026-04-01 10:30:53 +0200

    Extends safe / unsafe query testing

--- tests/unit/queries/safe-and-default-queries.spec.ts
+++ tests/unit/queries/safe-and-default-queries.spec.ts
@@ -5,13 +5,15 @@ import { createPagedFetchQuery, KontentSdkError } from "../../../lib/public_api.
 import { createFetchQuery } from "../../../lib/sdk/queries/fetch-sdk-query.js";
 import { createMutationQuery } from "../../../lib/sdk/queries/mutation-sdk-query.js";
 import type { FetchQuery, MutationQuery, PagedFetchQuery } from "../../../lib/sdk/sdk-models.js";
-import { getTestSdkInfo } from "../../../lib/testkit/testkit.utils.js";
+import { getTestHttpServiceWithJsonResponse, getTestSdkInfo } from "../../../lib/testkit/testkit.utils.js";
 
 type QueryTest = {
 	readonly name: string;
 	readonly getQueries: () => {
 		readonly unsafeQueries: readonly QueryCallback[];
+		readonly succeedingUnsafeQueries: readonly QueryCallback[];
 		readonly safeQueries: readonly QueryCallback[];
+		readonly succeedingSafeQueries: readonly QueryCallback[];
 	};
 };
 
@@ -21,6 +23,25 @@ type QueryCallback = {
 		| keyof FetchQuery<unknown, unknown, unknown>
 		| keyof PagedFetchQuery<unknown, unknown, unknown>
 		| keyof MutationQuery<unknown, unknown, unknown>;
+};
+
+const successfulBaseQueryConfig: Parameters<typeof createFetchQuery>[0] = {
+	zodSchema: z.null(),
+	request: {
+		url: "https://domain.com",
+	},
+	config: {
+		httpService: getTestHttpServiceWithJsonResponse({
+			statusCode: 200,
+			jsonResponse: null,
+		}),
+		responseValidation: {
+			enable: false,
+		},
+	},
+	sdkInfo: getTestSdkInfo(),
+	mapMetadata: () => ({}),
+	mapError: (error) => error,
 };
 
 const baseQueryConfig: Parameters<typeof createFetchQuery>[0] = {
@@ -58,11 +79,28 @@ const unsafeQueries: readonly QueryTest[] = [
 				},
 			});
 
+			const succeedingBaseQuery = createMutationQuery({
+				...successfulBaseQueryConfig,
+				method: "POST",
+				request: {
+					url: "https://domain.com",
+					body: null,
+				},
+			});
+
 			return {
 				unsafeQueries: [
 					{
 						callback: async () => {
 							await baseQuery.execute();
+						},
+						title: "execute",
+					},
+				],
+				succeedingUnsafeQueries: [
+					{
+						callback: async () => {
+							await succeedingBaseQuery.execute();
 						},
 						title: "execute",
 					},
@@ -75,6 +113,14 @@ const unsafeQueries: readonly QueryTest[] = [
 						title: "execute",
 					},
 				],
+				succeedingSafeQueries: [
+					{
+						callback: async () => {
+							await succeedingBaseQuery.executeSafe();
+						},
+						title: "executeSafe",
+					},
+				],
 			};
 		},
 	},
@@ -82,12 +128,21 @@ const unsafeQueries: readonly QueryTest[] = [
 		name: "FetchQuery",
 		getQueries: () => {
 			const baseQuery = createFetchQuery(baseQueryConfig);
+			const succeedingBaseQuery = createFetchQuery(successfulBaseQueryConfig);
 
 			return {
 				unsafeQueries: [
 					{
 						callback: async () => {
 							await baseQuery.fetch();
+						},
+						title: "fetch",
+					},
+				],
+				succeedingUnsafeQueries: [
+					{
+						callback: async () => {
+							await succeedingBaseQuery.fetch();
 						},
 						title: "fetch",
 					},
@@ -100,6 +155,14 @@ const unsafeQueries: readonly QueryTest[] = [
 						title: "fetchSafe",
 					},
 				],
+				succeedingSafeQueries: [
+					{
+						callback: async () => {
+							await succeedingBaseQuery.fetchSafe();
+						},
+						title: "fetchSafe",
+					},
+				],
 			};
 		},
 	},
@@ -108,6 +171,13 @@ const unsafeQueries: readonly QueryTest[] = [
 		getQueries: () => {
 			const baseQuery = createPagedFetchQuery({
 				...baseQueryConfig,
+				getNextPageData: () => {
+					return {};
+				},
+			});
+
+			const succeedingBaseQuery = createPagedFetchQuery({
+				...successfulBaseQueryConfig,
 				getNextPageData: () => {
 					return {};
 				},
@@ -135,6 +205,27 @@ const unsafeQueries: readonly QueryTest[] = [
 						title: "pages",
 					},
 				],
+				succeedingUnsafeQueries: [
+					{
+						callback: async () => {
+							await succeedingBaseQuery.fetchPage();
+						},
+						title: "fetchPage",
+					},
+					{
+						callback: async () => {
+							await succeedingBaseQuery.fetchAllPages();
+						},
+						title: "fetchAllPages",
+					},
+					{
+						callback: async () => {
+							for await (const _page of succeedingBaseQuery.pages()) {
+							}
+						},
+						title: "pages",
+					},
+				],
 				safeQueries: [
 					{
 						callback: async () => {
@@ -156,6 +247,27 @@ const unsafeQueries: readonly QueryTest[] = [
 						title: "pages",
 					},
 				],
+				succeedingSafeQueries: [
+					{
+						callback: async () => {
+							await succeedingBaseQuery.fetchPageSafe();
+						},
+						title: "fetchPageSafe",
+					},
+					{
+						callback: async () => {
+							await succeedingBaseQuery.fetchAllPagesSafe();
+						},
+						title: "fetchAllPagesSafe",
+					},
+					{
+						callback: async () => {
+							for await (const _page of succeedingBaseQuery.pagesSafe()) {
+							}
+						},
+						title: "pagesSafe",
+					},
+				],
 			};
 		},
 	},
@@ -163,15 +275,25 @@ const unsafeQueries: readonly QueryTest[] = [
 
 for (const unsafeQuery of unsafeQueries) {
 	describe(`Safety testing for ${unsafeQuery.name} query`, () => {
-		const { unsafeQueries, safeQueries } = unsafeQuery.getQueries();
+		const { unsafeQueries, succeedingUnsafeQueries, safeQueries, succeedingSafeQueries } = unsafeQuery.getQueries();
 		for (const unsafeQueryCallback of unsafeQueries) {
 			it(`Default '${unsafeQueryCallback.title}' query should throw an error`, async () => {
-				await expect(unsafeQueryCallback.callback()).rejects.toThrowError(KontentSdkError);
+				await expect(unsafeQueryCallback.callback()).rejects.toThrow(KontentSdkError);
+			});
+		}
+		for (const succeedingUnsafeQueryCallback of succeedingUnsafeQueries) {
+			it(`Default '${succeedingUnsafeQueryCallback.title}' query should not throw when successful`, async () => {
+				await expect(succeedingUnsafeQueryCallback.callback()).resolves.not.toThrow();
 			});
 		}
 		for (const safeQueryCallback of safeQueries) {
 			it(`Safe '${safeQueryCallback.title}' query should not throw an error`, async () => {
-				await expect(safeQueryCallback.callback()).resolves.not.toThrowError(KontentSdkError);
+				await expect(safeQueryCallback.callback()).resolves.not.toThrow(KontentSdkError);
+			});
+		}
+		for (const succeedingSafeQueryCallback of succeedingSafeQueries) {
+			it(`Safe '${succeedingSafeQueryCallback.title}' query should not throw when successful`, async () => {
+				await expect(succeedingSafeQueryCallback.callback()).resolves.not.toThrow();
 			});
 		}
 	});
